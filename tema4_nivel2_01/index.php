21eb30567dec59b140a9b3ef3d3681f77111788e
frankpulido <frankpulido@me.com>
2024-10-12 13:05:29 +0200

    refactored poker dice

--- tema4_nivel2_01/index.php
+++ tema4_nivel2_01/index.php
@@ -1,143 +1,48 @@
 <?php
-require "pokerdice.php";
-/*
-Frank Pulido - Tema 4 [POO1] - Nivel 2 - Ejercicio 1
+declare(strict_types=1);
+require "pokerDice.php";
 
-ENUNCIADO :
-Crea la clase PokerDice. Las caras de un dado de póquer tienen las siguientes figuras: As, K, Q, J, 7 y 8.
-Crea el método throw que no hace otra cosa que echar el dado, es decir, genera un valor aleatorio para el objeto al que se le aplica el método.
-Crea también el método shapeName, que diga cuál es la figura que ha salido en el último tirón del dado en cuestión.
-Realiza una aplicación que permita echar cinco dados de póquer a la vez.
-Además, programa el método getTotalThrows que debe mostrar el número total de tiradas entre todos los dados.
-
-RECURSOS :
-https://www.w3schools.com/php/func_array_shuffle.asp // reordeno los elementos y tomo el valor del primero con array_current()
-https://www.w3schools.com/php/func_array_current.asp // tomar primer valor del array (quizás requiere hacer un reset() previo para reordenar claves)
-https://www.w3schools.com/php/func_array_fill.asp // crear array de 5 dados
-https://www.w3schools.com/php/func_array_walk.asp // ejecutar la misma función con 5 dados del array
+/* README RUBÉN
+Espero haberlo conseguido esta vez.
+Durante la tutoría que hicimos al terminar el examen 2 (2024 10 10) te estuve preguntado sobre el uso del SELF y del modificador STATIC.
+Estaba pensando en rehacer los ejercicios de POO1.
+Empiezo con el más sencillo.
+Me di cuenta de que al acabar de estudiar ARRAYS me dediqué a poner en práctica lo recién aprendido usando todas las funciones posibles de ARRAYS y si bien conseguí practicar mucho el tema anterior, no conseguí el objetivo del ejercicio siguiendo la metodología del array KISS.
+$KISS = ['Keep', 'It', 'Simple', 'Stupid'];
+*** IMPORTANTE  : Estadísticamente, las probabilidades de obtener una combinación cualquiera de 5 resultados lanzando un set de 5 dados es la misma que la de obtener la misma combinación lanzando un único dado 5 veces.
 */
 
-$dado = new PokerDice();
-$set5Dados = [];
-
-for ($i = 0; $i <= 4; $i++) {
-    $set5Dados[] = clone($dado);
-}
-$jugada = []; // $jugada almacenará el outcome de un lanzamiento de 5 dados : $set5Dados.
-$jugadas = []; // $almacenará todos los outcomes de cada lanzamiento de los 5 dados.
-
-$opcion = -1; // entrada del usuario, menú principal
-
-echo "\nRubén : tengo un \"side effect\" al inicio (el require a la Clase PokerDice) y un \"declaration\" al final (método getTotalThrows) que creé en index porque me pareció demasiado\n";
-echo "crear una nueva clase para el set de dados... Todos los jugadores pueden jugar con el mismo set. Al parecer esto es contrario al PSR-1: Basic Coding Standard... puedes orientarme?...\n\n";
+$option = -1;
 
 do {
-    echo "\nMenú de usuario :\n[1] Presentación del dado de Poker Dice y las funciones \"throwDice\" y \"shapeName\".\n[2] Presentación del set de 5 dados y la jugada.\n[3] Practica el lanzamiento y almacena los resultados con \"getTotalThrows\".\n[0] Salir.\n\n";
-    $opcion = (int) readline("Que opción escoges? (1 al 3, salir : 0)\n");
-    switch ($opcion) {
+    echo PHP_EOL;
+    echo "1. Throw PokerDice." . PHP_EOL . "2. Throw PokerDice Set." . PHP_EOL . "3. Get the times a single dice has been thrown alone or as part of the set (5 dice)." . PHP_EOL . "4. Get the times the set of five dice has been thrown" . PHP_EOL;
+    echo PHP_EOL;
+    $option = (int) readline("Your option : ");
+    echo PHP_EOL;
+    switch ($option) {
+        case 0 :
+            echo "See you later, alligator!!!..." . PHP_EOL;
+            break;
         case 1 :
-            echo "\nOs presentamos el dado usado en PokerDice :\n";
-            var_dump ($dado);
-            echo "\nVamos a lanzarlo, veamos como ha cambiado el orden de las caras del dado :\n";
-            $dado->throwDice();
-            var_dump ($dado);
-            echo "\nEl resultado del lanzamiento será la cara que corresponde a la posición [0] :\n";
-            echo $dado->shapeName();
-            echo "\n\n";
+            echo PokerDice::shapeNameDice();
+            echo PHP_EOL;
             break;
         case 2 :
-            echo "En cada jugada de \"Poker Dice\" deben lanzarse 5 dados.\nPresentamos el set para que comprobéis las caras (el set se desprecinta delante del jugador, su peso y centro de gravedad están certificados) :\n";
-            echo "\n";
-            var_dump($set5Dados);
-            echo "\n\n";
-            echo "Lancemos!!!\n\n";
-            for ($i = 0; $i < count($set5Dados); $i++) {
-                $set5Dados[$i]->throwDice();
-            }
-            var_dump($set5Dados);
-            echo "\nLa jugada tiene por resultado la posición [0] de cada uno de los 5 dados :\n";
-            for ($i = 0; $i < count($set5Dados); $i++) {
-                $jugada[] = $set5Dados[$i]->shapeName();
-            }
-            echo "\n";
-            print_r($jugada);
-            echo "\n";
+            echo PokerDice::shapeNameSet();
+            echo PHP_EOL;
             break;
         case 3 :
-            // Crear la clase Jugador que tenga por atributos $nombre, $set5Dados y $jugadas[], además del método getTotalThrows.
-            // Hacer el juego creando X instancias de la clase Jugador y ejecutando getTotalThrouws en un foreach del array de estas instancias.
-            echo "Ya sabes como funcionan el objeto \"dado\" y el array \"set5dados\" que almacena 5 clones de \"dado\", así como los métodos \"throwDice\" y \"shapeName\" de la Clase PokerDice.\n";
-            echo "Ahora usaremos el método \"getTotalThrows\" que te permitirá lanzar las veces que quieras y te mostrará el total de lanzamientos y su resultado.\n";
-            echo "DEBERES : modificarlo para un juego de X jugadores\n\n";
-            
-            unset($jugadas);
-            do {
-                $jugar = (int) readline("Quieres lanzar los dados?\n[1] Si\n[0] No\n");
-                switch ($jugar) {
-                    case 0 :
-                        echo "\nEsperamos que hayas tenido suerte, hasta la próxima!!\n";
-                        break;
-                    case 1 :
-                        getTotalThrows($set5Dados, $jugadas); // La función está definida para el paso de variables por referencia
-                        break;
-                    default :
-                        echo "Debes seleccionar una opción válida.\n\n";
-                        break;
-                }
-            } while ($jugar!= 0);
-            
-            echo "\nHas lanzado el set de 5 dados un total de " . count($jugadas) . " veces en esta práctica.\n\n"; // En el método getTotalThrows la variable $totalThrows es static
-            echo "El resultado de los " . count($jugadas) . " lanzamientos :\n\n"; // $totalThrows debe arrojar el mismo valor que count($jugadas) que se pasó "por referencia"
-            print_r($jugadas);
-            
-            //getTotalThrows($set5Dados);
-            echo "\n\n";
+            echo PokerDice::getTotalThrowsDice();
+            echo PHP_EOL;
             break;
-        case 0 :
-            echo "Salida del sistema.\n";
-            echo "\n\n";
+        case 4 :
+            echo PokerDice::getTotalThrowsSet();
+            echo PHP_EOL;
             break;
         default :
-        echo "Debes seleccionar una opción válida.\n";
-            echo "\n\n";
             break;
     }
-} while ($opcion != 0);
-
-
-echo 'DUDA : Debería el atributo único $dice ser de tipo PRIVATE, en lugar de protected???
-Estudiar con detenimiento la función getTotalThrows(). Necesité pasar las variables $set5Dados y $jugadas "por referencia" : &$set5Dados y &$jugadas.
-Necesitaba que el valor de ambas (variables globales) se viese afectado durante la ejecución del método.
-IMPORTANTE : no existe "return", sencillamente afecto sus valores. Hago un echo para verificar la cuenta de la variable static.
-Revisar TODO el programa.
-Recursos :
-https://www.w3schools.com/php/php_superglobals_globals.asp
-Scroll to "Passing Arguments by Reference" :
-https://www.w3schools.com/php/php_functions.asp';
-echo "\n";
-
-
-function getTotalThrows(&$set5Dados, &$jugadas) { // pasamos las variables "por referencia" y no hacemos return. En "case 3" hacemos unset($jugadas)
-    static $totalThrows = 0; // al ser static esta variable acumulará el total de lanzamientos usando la opción 3 para toda la sesión de usuario
-    for ($i = 0; $i < count($set5Dados); $i++) {
-        $set5Dados[$i]->throwDice();
-        $jugada[] = $set5Dados[$i]->shapeName();
-    }
-    $totalThrows++;
-    $jugadas[] = $jugada;
-    echo "En esta práctica usted ha lanzado " . count($jugadas) . " veces.\nDurante la sesión de usuario usted ha lanzado un total de ". $totalThrows . " veces.\n";
-}
-
-/*
-DUDA : Debería el atributo único $dice ser de tipo PRIVATE, en lugar de protected???
-Estudiar con detenimiento la función getTotalThrows(). Necesité pasar las variables $set5Dados y $jugadas "por referencia" : &$set5Dados y &$jugadas.
-Necesitaba que el valor de ambas (variables globales) se viese afectado durante la ejecución del método.
-IMPORTANTE : no existe "return", sencillamente afecto sus valores.
-Revisar TODO el programa.
-Recursos :
-https://www.w3schools.com/php/php_superglobals_globals.asp
-Scroll to "Passing Arguments by Reference" :
-https://www.w3schools.com/php/php_functions.asp
-*/
+} while($option != 0);
 
 ?>
--- tema4_nivel2_01/pokerdice.php
+++ tema4_nivel2_01/pokerdice.php
@@ -1,43 +1,42 @@
 <?php
-class PokerDice {
+final class PokerDice {
 
     // Atributo
-    protected $dice = []; // No quiero que sea posible alterar los valores desde la aplicación (quizás deba usar private?)
+    private static $dice = ['As', 'K', 'J', '7', '8'];
+    private static int $totalThrowsDice = 0;
+    private static int $totalThrowsSet = 0;
+    // Constructor : NOT NEEDED
 
-    // Constructor
-    public function __construct($dice = ['As', 'K', 'J', '7', '8']) {
-        $this->dice = $dice;
+    // Getters
+    public static function getDice() {
+        return self::$dice;
+    }
+    public static function getTotalThrowsDice() : int {
+        return self::$totalThrowsDice;
+    }
+    public static function getTotalThrowsSet() : int {
+        return self::$totalThrowsSet;
     }
 
-    // Getter
-    public function getDice() {
-        return $this->dice;
+    // "throwDice" (tirar el dado)
+    public static function throwDice() : int {
+        self::$totalThrowsDice++;
+        return random_int(0,4);
     }
 
-    // Setter : NO HACEMOS SETTER, el dado no debe ser modificado.
-
-    // MÉTODOS PROPIOS DE LA CLASE : throwDice() y shapeName()
-
-    // Función "throwDice" (tirar el dado) : hace un shuffle para cambiar aleatoriamente el orden de los elementos y un reset para reindexar.
-    public function throwDice() {
-        shuffle($this->dice);
-        reset($this->dice);
-        //return $this->dice;
-        /* 
-        podríamos cambiar la app para que sólo haga el shuffle, sin return, y evitarnos quizás variables auxiliares que sólo sirven para presentar el estado de los dados
-        por pantalla y mostrar como funciona el programa.
-        */
+    // Función "ShapeNameDice" (resultado de la jugada)
+    public static function shapeNameDice() : string {
+        return "The outcome of the dice throw is : " . self::$dice[self::throwDice()];
     }
 
-    // Función "ShapeName" (resultado de la jugada) : reindexa $keys (sin alterar orden de $values) y me devuelve $dice[0]
-    public function shapeName() {
-        reset($this->dice);
-        /*
-        con el reset ya hecho en throwDice() después del shuffle, este otro es redundante, pero de momento lo dejo hasta haber hecho el juego con varios jugadores
-        y estar seguro de que no es necesario.
-        */
-        return current($this->dice);
+    // Función "ShapeNameSet" (resultado de tirar el set de 5 dados)
+    public static function shapeNameSet() : string {
+        $reply = "";
+        self::$totalThrowsSet++;
+        for($i = 1; $i <= 5; $i++) {
+            $reply = $reply . self::shapeNameDice() . PHP_EOL;
+        }
+        return $reply;
     }
 }
-
 ?>
